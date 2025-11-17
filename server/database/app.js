c7c1337d045b01c656d17ffd22a4d30ff3c59687
jk_austin <jessekorff@gmail.com>
2025-11-17 15:08:04 -0500

    linting app.js

--- server/database/app.js
+++ server/database/app.js
@@ -92,20 +92,20 @@ app.get('/fetchDealer/:id', async (req, res) => {
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
-  data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } );
+  const data = JSON.parse(req.body);
+  const documents = await Reviews.find().sort({ id: -1 });
   let new_id = documents[0].id + 1;
 
   const review = new Reviews({
 		"id": new_id,
 		"name": data.name,
-		"dealership": data.dealership.,
+		"dealership": data.dealership,
 		"review": data.review,
 		"purchase": data.purchase,
 		"purchase_date": data.purchase_date,
 		"car_make": data.car_make,
 		"car_model": data.car_model,
-		"car_year": data.car_year,
+		"car_year": data.car_year
 	});
 
   try {
@@ -113,7 +113,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     res.json(savedReview);
   } catch (error) {
 		console.log(error);
-    res.status(500).json({ error: 'Error inserting review' });
+    	res.status(500).json({ error: 'Error inserting review' });
   }
 });
 
