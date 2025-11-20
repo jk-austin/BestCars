f248d0bfda4583fcd3e36335a5e2cbf02cb281fb
jessekorff <jessekorff@gmail.com>
2025-11-20 12:01:55 -0500

    clear sandbox

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,7 +22,9 @@ REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend', 'build')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# In production, the secret key would not be spelled out but
+# would be read from the environment:
+# SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-for-dev')
 SECRET_KEY =\
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
